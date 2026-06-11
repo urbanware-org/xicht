57209331100ce2421b625d394bcd107580961db2
urbanware-org <github@urbanware.org>
2026-06-12 00:12:21 +0200

    Add image rendering class

---
+++ Source/Xicht/ImageRenderer.cs
@@ -0,0 +1,31 @@
+﻿/*
+    Xicht - Visual CPU load monitoring tool
+    Image rendering class
+    Copyright (c) 2026 by Ralf Kilian
+    Distributed under the MIT License (https://opensource.org/licenses/MIT)
+
+    GitHub: https://github.com/urbanware-org/xicht
+    GitLab: https://gitlab.com/urbanware-org/xicht
+*/
+
+using System.Drawing;
+using System.Windows.Forms;
+using System.Drawing.Drawing2D;
+
+public class SmoothImageRenderer : ToolStripProfessionalRenderer
+{
+    protected override void OnRenderItemImage(ToolStripItemImageRenderEventArgs e)
+    {
+        if (e.Image != null)
+        {
+            Graphics g = e.Graphics;
+            g.CompositingQuality = CompositingQuality.HighQuality;
+            g.InterpolationMode = InterpolationMode.HighQualityBicubic;
+            g.PixelOffsetMode = PixelOffsetMode.HighQuality;
+            g.SmoothingMode = SmoothingMode.AntiAlias;
+
+            Rectangle bounds = e.ImageRectangle;
+            g.DrawImage(e.Image, bounds);
+        }
+    }
+}
