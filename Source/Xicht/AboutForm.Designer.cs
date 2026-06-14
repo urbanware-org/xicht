891cc8753c5a718d814d19eb7abaa5dac18309c3
urbanware-org <github@urbanware.org>
2026-06-14 11:35:18 +0200

    Revise code in general (negligible changes)

--- Source/Xicht/AboutForm.Designer.cs
+++ Source/Xicht/AboutForm.Designer.cs
@@ -35,19 +35,19 @@
             this.lblCopyright = new System.Windows.Forms.Label();
             this.lblVersion = new System.Windows.Forms.Label();
             this.lblLicense = new System.Windows.Forms.Label();
-            this.lnkLicense = new System.Windows.Forms.LinkLabel();
             this.lblGraphics = new System.Windows.Forms.Label();
             this.pbReinchard = new System.Windows.Forms.PictureBox();
             this.pbAboutLogo = new System.Windows.Forms.PictureBox();
-            this.lnkGitHub = new System.Windows.Forms.LinkLabel();
             this.lblHosted = new System.Windows.Forms.Label();
             this.lnkGitLab = new System.Windows.Forms.LinkLabel();
+            this.lnkGitHub = new System.Windows.Forms.LinkLabel();
+            this.lnkLicense = new System.Windows.Forms.LinkLabel();
             ((System.ComponentModel.ISupportInitialize)(this.pbReinchard)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pbAboutLogo)).BeginInit();
             this.SuspendLayout();
-            // 
+            //
             // btnClose
-            // 
+            //
             this.btnClose.Location = new System.Drawing.Point(366, 12);
             this.btnClose.Name = "btnClose";
             this.btnClose.Size = new System.Drawing.Size(97, 30);
@@ -55,9 +55,9 @@
             this.btnClose.Text = "Close";
             this.btnClose.UseVisualStyleBackColor = true;
             this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
-            // 
+            //
             // lblXicht
-            // 
+            //
             this.lblXicht.AutoSize = true;
             this.lblXicht.Font = new System.Drawing.Font("Segoe UI", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblXicht.Location = new System.Drawing.Point(82, 12);
@@ -65,9 +65,9 @@
             this.lblXicht.Size = new System.Drawing.Size(64, 30);
             this.lblXicht.TabIndex = 2;
             this.lblXicht.Text = "Xicht";
-            // 
+            //
             // lblDescription
-            // 
+            //
             this.lblDescription.AutoSize = true;
             this.lblDescription.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblDescription.Location = new System.Drawing.Point(84, 42);
@@ -75,9 +75,9 @@
             this.lblDescription.Size = new System.Drawing.Size(181, 15);
             this.lblDescription.TabIndex = 3;
             this.lblDescription.Text = "Visual CPU load monitoring tool";
-            // 
+            //
             // lblCopyright
-            // 
+            //
             this.lblCopyright.AutoSize = true;
             this.lblCopyright.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblCopyright.Location = new System.Drawing.Point(84, 85);
@@ -85,9 +85,9 @@
             this.lblCopyright.Size = new System.Drawing.Size(60, 15);
             this.lblCopyright.TabIndex = 4;
             this.lblCopyright.Text = "Copyright";
-            // 
+            //
             // lblVersion
-            // 
+            //
             this.lblVersion.AutoSize = true;
             this.lblVersion.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblVersion.Location = new System.Drawing.Point(84, 61);
@@ -95,9 +95,9 @@
             this.lblVersion.Size = new System.Drawing.Size(46, 15);
             this.lblVersion.TabIndex = 5;
             this.lblVersion.Text = "Version";
-            // 
+            //
             // lblLicense
-            // 
+            //
             this.lblLicense.AutoSize = true;
             this.lblLicense.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblLicense.Location = new System.Drawing.Point(84, 109);
@@ -105,21 +105,9 @@
             this.lblLicense.Size = new System.Drawing.Size(119, 15);
             this.lblLicense.TabIndex = 6;
             this.lblLicense.Text = "Distributed under the";
-            // 
-            // lnkLicense
-            // 
-            this.lnkLicense.AutoSize = true;
-            this.lnkLicense.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lnkLicense.Location = new System.Drawing.Point(201, 109);
-            this.lnkLicense.Name = "lnkLicense";
-            this.lnkLicense.Size = new System.Drawing.Size(70, 15);
-            this.lnkLicense.TabIndex = 7;
-            this.lnkLicense.TabStop = true;
-            this.lnkLicense.Text = "MIT License";
-            this.lnkLicense.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkLicense_LinkClicked);
-            // 
+            //
             // lblGraphics
-            // 
+            //
             this.lblGraphics.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblGraphics.Location = new System.Drawing.Point(84, 157);
             this.lblGraphics.Name = "lblGraphics";
@@ -128,9 +116,9 @@
             this.lblGraphics.Text = "Special thanks to Reinchard who designed and kindly provided the high resolution " +
     "face images used by default.";
             this.lblGraphics.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            // 
+            //
             // pbReinchard
-            // 
+            //
             this.pbReinchard.Image = global::Xicht.Properties.Resources.faces_reinchard;
             this.pbReinchard.Location = new System.Drawing.Point(411, 157);
             this.pbReinchard.Name = "pbReinchard";
@@ -138,9 +126,9 @@
             this.pbReinchard.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pbReinchard.TabIndex = 9;
             this.pbReinchard.TabStop = false;
-            // 
+            //
             // pbAboutLogo
-            // 
+            //
             this.pbAboutLogo.Image = global::Xicht.Properties.Resources.Xicht_about;
             this.pbAboutLogo.Location = new System.Drawing.Point(12, 12);
             this.pbAboutLogo.Name = "pbAboutLogo";
@@ -148,21 +136,9 @@
             this.pbAboutLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
             this.pbAboutLogo.TabIndex = 0;
             this.pbAboutLogo.TabStop = false;
-            // 
-            // lnkGitHub
-            // 
-            this.lnkGitHub.AutoSize = true;
-            this.lnkGitHub.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lnkGitHub.Location = new System.Drawing.Point(217, 133);
-            this.lnkGitHub.Name = "lnkGitHub";
-            this.lnkGitHub.Size = new System.Drawing.Size(45, 15);
-            this.lnkGitHub.TabIndex = 11;
-            this.lnkGitHub.TabStop = true;
-            this.lnkGitHub.Text = "GitHub";
-            this.lnkGitHub.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkGitHub_LinkClicked);
-            // 
+            //
             // lblHosted
-            // 
+            //
             this.lblHosted.AutoSize = true;
             this.lblHosted.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblHosted.Location = new System.Drawing.Point(84, 133);
@@ -170,9 +146,9 @@
             this.lblHosted.Size = new System.Drawing.Size(201, 15);
             this.lblHosted.TabIndex = 10;
             this.lblHosted.Text = "This project is hosted on               and";
-            // 
+            //
             // lnkGitLab
-            // 
+            //
             this.lnkGitLab.AutoSize = true;
             this.lnkGitLab.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lnkGitLab.Location = new System.Drawing.Point(282, 133);
@@ -182,9 +158,33 @@
             this.lnkGitLab.TabStop = true;
             this.lnkGitLab.Text = "GitLab";
             this.lnkGitLab.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkGitLab_LinkClicked);
-            // 
+            //
+            // lnkGitHub
+            //
+            this.lnkGitHub.AutoSize = true;
+            this.lnkGitHub.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnkGitHub.Location = new System.Drawing.Point(217, 133);
+            this.lnkGitHub.Name = "lnkGitHub";
+            this.lnkGitHub.Size = new System.Drawing.Size(45, 15);
+            this.lnkGitHub.TabIndex = 11;
+            this.lnkGitHub.TabStop = true;
+            this.lnkGitHub.Text = "GitHub";
+            this.lnkGitHub.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkGitHub_LinkClicked);
+            //
+            // lnkLicense
+            //
+            this.lnkLicense.AutoSize = true;
+            this.lnkLicense.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lnkLicense.Location = new System.Drawing.Point(201, 109);
+            this.lnkLicense.Name = "lnkLicense";
+            this.lnkLicense.Size = new System.Drawing.Size(70, 15);
+            this.lnkLicense.TabIndex = 7;
+            this.lnkLicense.TabStop = true;
+            this.lnkLicense.Text = "MIT License";
+            this.lnkLicense.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.lnkLicense_LinkClicked);
+            //
             // AboutForm
-            // 
+            //
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(474, 223);
@@ -227,11 +227,11 @@
         private System.Windows.Forms.Label lblCopyright;
         private System.Windows.Forms.Label lblVersion;
         private System.Windows.Forms.Label lblLicense;
-        private System.Windows.Forms.LinkLabel lnkLicense;
         private System.Windows.Forms.Label lblGraphics;
         private System.Windows.Forms.PictureBox pbReinchard;
-        private System.Windows.Forms.LinkLabel lnkGitHub;
         private System.Windows.Forms.Label lblHosted;
         private System.Windows.Forms.LinkLabel lnkGitLab;
+        private System.Windows.Forms.LinkLabel lnkGitHub;
+        private System.Windows.Forms.LinkLabel lnkLicense;
     }
 }
